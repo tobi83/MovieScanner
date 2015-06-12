f1717fa6017d045967e217099a2e80692d9ec6dc
Tobi <tobias@tmack.de>
2015-06-12 18:34:15 +0200

    Erster Upload

---
+++ app/src/main/java/de/tmack/filme/moviescanner/MainActivity.java
@@ -0,0 +1,136 @@
+package de.tmack.filme.moviescanner;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Button btn_Scan;
+    private TextView tv_Barcode;
+    private TextView tv_Typ;
+    private TextView tv_Standort;
+    private TextView tv_Titel;
+    private WebView webView;
+
+    private String barcode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void startScan (View view) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            //String barcode;
+            String typ;
+
+            barcode = scanResult.getContents();
+            typ = scanResult.getFormatName();
+
+            /*
+            tv_Barcode = (TextView) findViewById(R.id.tv_Barcode);
+            tv_Typ = (TextView) findViewById(R.id.tv_Typ);
+            tv_Standort = (TextView) findViewById(R.id.tv_Standort);
+            tv_Titel = (TextView) findViewById(R.id.tv_Titel);
+            */
+            /*
+            tv_Barcode.setText("Barcode: " + barcode);
+            tv_Typ.setText("Barcode-Typ: " + typ);
+            tv_Titel.setText("Titel: Eyja Fjalla Joekull");
+            tv_Standort.setText("Standort: Regal A13");
+*/
+            WebView webview = new WebView(this);
+            setContentView(webview);
+
+            webview.setWebViewClient(new WebViewClient());
+            webview.setVisibility(View.VISIBLE);
+            webview.loadUrl("http://tmack.de/filmdatenbank/get_movie.php?barcode=" + barcode);
+        }
+    }
+
+    public void FilmSuchen(View view) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);*/
+
+        switch (item.getItemId()) {
+            case R.id.quit:
+                System.exit(0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
